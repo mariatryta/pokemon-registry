a4a3f4107e932b6862df933bba1640265ee90c8a
Jaja <mariatryta@gmail.com>
2022-05-24 14:58:07 +0200

    test keyboard navigation & adjust element focus

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -1,13 +1,7 @@
 <template>
   <button
-    class="px-5 py-3 font-medium rounded bg-indigo-400 text-white hover:z-10 focus:outline-none focus:ring-indigo-700 focus:z-10 hover:ring-2 focus:ring-2 hover:ring-indigo-700"
+    class="px-5 py-3 font-medium rounded bg-indigo-400 text-white hover:z-10 focus:outline-none focus:ring-indigo-700 focus:z-10 hover:ring-2 focus:ring-2 hover:ring-indigo-700 disabled:bg-slate-400 disabled:cursor-not-allowed"
   >
     <slot></slot>
   </button>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
--- src/components/VSelect.vue
+++ src/components/VSelect.vue
@@ -3,7 +3,7 @@
     <span class="sr-only"> Sort </span>
 
     <select
-      class="py-3 px-5 font-medium border-gray-200 focus:outline-none focus:border-indigo-600 focus:z-10 hover:bg-gray-50 focus:ring-0 w-full"
+      class="py-3 px-5 font-medium border-gray-200 focus:outline-none focus:border-indigo-600 focus:z-10 hover:bg-gray-50 focus:ring-2 w-full"
       id="sort"
       name="sort"
       :value="value"
